bafb02bc66ba135dbc60aa165a9b7b8455b42c8f
Joshua Dadson <joshua.dadson@ibm.com>
2025-11-23 21:39:47 -0600

    remove before files

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   async rewrites() {
-    return {
-      beforeFiles: [
+    return  [
         {
           source: '/',
           destination: '/dashboard'
         }
-      ]
-    }
+      ] 
   }
 };
 
