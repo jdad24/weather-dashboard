0424947a0852e4373f48a3546323f0bdc3db4189
Joshua Dadson <joshua.dadson@ibm.com>
2025-11-28 23:57:35 -0600

    swap overflow axis

--- src/app/_components/weather-table.tsx
+++ src/app/_components/weather-table.tsx
@@ -90,7 +90,7 @@ export default function WeatherTable({ data = { maxTemperatures: {}, minTemperat
 
     return (
         <div className={cn(
-            "rounded-3xl shadow-black shadow-lg opacity-90 overflow-x-auto overflow-y-clip",
+            "rounded-3xl shadow-black shadow-lg opacity-90 overflow-y-auto overflow-x-clip",
             {
                 'h-70 lg:h-auto w-auto': open,
                 'h-15 w-auto': !open
