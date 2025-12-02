cd6f3d87f86a22eb777ee7162d4d2bc3ac315779
Joshua Dadson <joshua.dadson@ibm.com>
2025-12-02 16:36:02 -0600

    add current_temp column

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -4,7 +4,7 @@ import { neon } from '@neondatabase/serverless';
 
 export async function addWeatherRecord(description: string, city: string, country: string, current_temp: string) {
     const sql = neon(`${process.env.DATABASE_URL}`);
-    await sql`INSERT INTO weather_visits (weather_description, city, country) 
+    await sql`INSERT INTO weather_visits (weather_description, city, country, current_temp) 
     VALUES (${description}, ${city}, ${country}, ${current_temp})`
         
     
