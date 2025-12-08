b2edb448202123c53c9d874b0efc7504170e61ea
Joshua  Dadson <joshuadadson@Joshuas-MacBook-Air.local>
2025-12-07 21:46:29 -0600

    nav complete?

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -3,14 +3,16 @@ import Link
 
 export default function Navbar() {
     return (
-        <nav className="bg-gray-900 opacity-90 text-black h-screen min-w-40 flex flex-col sticky top-0">
-            <div className="bg-gray-900 text-white lg:text-3xl flex flex-row justify-center items-center h-20">JD</div>
-            <Link href="/dashboard">
-                <button className="w-full h-20 bg-white hover:bg-black hover:text-white hover:cursor-pointer border-b-1 font-bold">Forecast</button>
-            </Link>
-            <Link href="/archive">
-                <button className="w-full h-20 bg-white hover:bg-black hover:text-white hover:cursor-pointer font-bold">Archive</button>
-            </Link>
+        <nav className="bg-gray-900 opacity-90 text-black h-15 min-w-screen lg:h-screen lg:min-w-40 flex flex-row lg:flex-col sticky top-0">
+            <div className="bg-gray-900 text-white lg:text-3xl flex flex-row justify-center items-center h-full lg:h-15 ml-5 mr-5 lg:m-0">JD</div>
+            <div className="flex flex-row lg:flex-col w-full lg:h-auto">
+                <Link href="/dashboard" className="w-full">
+                    <button className="w-full h-full lg:h-20 bg-white hover:bg-black hover:text-white hover:cursor-pointer border-r-1 lg:border-b-1 font-bold">Forecast</button>
+                </Link>
+                <Link href="/archive" className="w-full">
+                    <button className="w-full h-full lg:h-20 bg-white hover:bg-black hover:text-white hover:cursor-pointer font-bold">Archive</button>
+                </Link>
+            </div>
         </nav>
     )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-row`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col lg:flex-row`}
       >        
           <Navbar/>
           {children}
